8af18eece3fa72acabe805a2208f9620185606f4
Romifff <romain.magne@edhec.com>
2023-08-07 20:09:28 +0200

    commit

--- index.js
+++ index.js
@@ -167,10 +167,6 @@ app.get('/api/users/:_id/logs/:from?/:to?/:limit?', async (req, res) => {
   }
 });
 
-// You can add from, to and limit parameters to a GET /api/users/:_id/logs request 
-// to retrieve part of the log of any user. from and to are dates in yyyy-mm-dd format. 
-//limit is an integer of how many logs to send back.
-
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
